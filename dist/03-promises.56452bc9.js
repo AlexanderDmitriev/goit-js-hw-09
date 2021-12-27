576c632c9ab8d7c9bc40d0bac982c72d01c06ce1
AlexanderDmitriev <alex9110077@gmail.com>
2021-12-28 01:31:33 +0200

    1

--- dist/03-promises.56452bc9.js
+++ dist/03-promises.56452bc9.js
@@ -118,6 +118,68 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"js/03-promises.js":[function(require,module,exports) {
+/* Задание 3 - генератор промисов
+  Выполняй это задание в файлах 03-promises.html и 03-promises.js. 
+Посмотри демо видео работы генератора промисов.
+
+  В HTML есть разметка формы, в поля которой пользователь будет вводить первую задержку в миллисекундах, 
+  шаг увеличения задержки для каждого промиса после первого и количество промисов которое необходимо создать.
+
+<form class="form">
+  <label>
+    First delay (ms)
+    <input type="number" name="delay" required />
+  </label>
+  <label>
+    Delay step (ms)
+    <input type="number" name="step" required />
+  </label>
+  <label>
+    Amount
+    <input type="number" name="amount" required />
+  </label>
+  <button type="submit">Create promises</button>
+</form>
+
+  Напиши скрипт, который при сабмите формы вызывает функцию createPromise(position, delay) столько раз, 
+сколько ввели в поле amount. При каждом вызове передай ей номер создаваемого промиса (position) и 
+задержку учитывая введенную пользователем первую задержку (delay) и шаг (step).
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
+
+  Дополни код функции createPromise так, чтобы она возвращала один промис, 
+который выполянется или отклоняется через delay времени. Значением промиса должен быть объект, 
+в котором будут свойства position и delay со значениями одноименных параметров. 
+Используй начальный код функции для выбора того, что нужно сделать с промисом - выполнить или отклонить.
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+Библиотека уведомлений
+
+Для отображения уведомлений пользователю вместо console.log() используй библиотеку notiflix.
+
+ */
+//================= Инициализация  =====================================//
+const refs = {
+  delayField: document.querySelector('.form input'),
+  stepField: document.querySelector('.form').children[1].firstElementChild,
+  amountField: document.querySelector('.form').children[2].firstElementChild,
+  submitButton: document.querySelector('.form button')
+}; //================= Методы  =====================================//
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
@@ -125,6 +187,16 @@ function createPromise(position, delay) {
   } else {// Reject
     }
 }
+
+const submitButtonHandler = event => {
+  event.preventDefault();
+  console.log(refs.delayField.value);
+  console.log(refs.stepField.value);
+  console.log(refs.amountField.value);
+}; //================= Actions  =====================================//
+
+
+refs.submitButton.addEventListener('click', submitButtonHandler);
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -153,7 +225,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54295" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56414" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,30 @@
+/* Задание 3 - генератор промисов
+  Выполняй это задание в файлах 03-promises.html и 03-promises.js. 
+Посмотри демо видео работы генератора промисов.
+
+  В HTML есть разметка формы, в поля которой пользователь будет вводить первую задержку в миллисекундах, 
+  шаг увеличения задержки для каждого промиса после первого и количество промисов которое необходимо создать.
+
+<form class="form">
+  <label>
+    First delay (ms)
+    <input type="number" name="delay" required />
+  </label>
+  <label>
+    Delay step (ms)
+    <input type="number" name="step" required />
+  </label>
+  <label>
+    Amount
+    <input type="number" name="amount" required />
+  </label>
+  <button type="submit">Create promises</button>
+</form>
+
+  Напиши скрипт, который при сабмите формы вызывает функцию createPromise(position, delay) столько раз, 
+сколько ввели в поле amount. При каждом вызове передай ей номер создаваемого промиса (position) и 
+задержку учитывая введенную пользователем первую задержку (delay) и шаг (step).
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +33,47 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+  Дополни код функции createPromise так, чтобы она возвращала один промис, 
+который выполянется или отклоняется через delay времени. Значением промиса должен быть объект, 
+в котором будут свойства position и delay со значениями одноименных параметров. 
+Используй начальный код функции для выбора того, что нужно сделать с промисом - выполнить или отклонить.
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+Библиотека уведомлений
+
+Для отображения уведомлений пользователю вместо console.log() используй библиотеку notiflix.
+
+ */
+//================= Инициализация  =====================================//
+const refs = {
+  delayField : document.querySelector('.form input'),
+  stepField : document.querySelector('.form').children[1].firstElementChild,
+  amountField : document.querySelector('.form').children[2].firstElementChild,
+  submitButton : document.querySelector('.form button')
+}
+//================= Методы  =====================================//
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
+
+const submitButtonHandler = (event) => {
+  event.preventDefault();
+  console.log(refs.delayField.value);
+  console.log(refs.stepField.value);
+  console.log(refs.amountField.value);
+}
+//================= Actions  =====================================//
+refs.submitButton.addEventListener('click' , submitButtonHandler);
